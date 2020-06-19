10919aa438d0170e948e0dfa9d004c686440d9d2
je.osorio <juliyanes530@gmail.com>
2020-06-19 12:59:29 -0500

    formulario de ingreso de un nuevo whis implementado

---
+++ angular-whislist/src/app/models/destino-viaje.model.ts
@@ -0,0 +1,9 @@
+export class DestinoViaje{
+  nombre:string;
+  imagenUrl:string;
+
+  constructor(  nombreP:string, imagenUrlP:string){
+    this.nombre = nombreP;
+    this.imagenUrl = imagenUrlP;
+  }
+}
